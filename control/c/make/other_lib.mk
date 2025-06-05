a1714161d8835af3218901da9f0e6fc33ce49b3e
ahsanww <99119708+ahsanww@users.noreply.github.com>
2025-06-05 13:41:39 +0700

    Update other_lib.mk

--- control/c/make/other_lib.mk
+++ control/c/make/other_lib.mk
@@ -1,5 +1,4 @@
-OTHER_LIBS += -lpaho-mqttpp3 -lpaho-mqtt3a -lpaho-mqtt3c
-OTHER_LIBS += -lyyjson
+OTHER_LIBS += -lpaho-mqtt3a -lpaho-mqtt3c
 
 MQTT_CLIENT_BINARY = mqtt_client.c
 LOG_LIB_BINARY = log.c
